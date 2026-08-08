2e2dd35c0934bd91c48beb85740c05a076d3c7df
nadabomuhammadjr-hub <muhammadbaimari@gmail.com>
2026-08-08 07:28:37 -0700

    fixed firebase-config.js syntax

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,11 @@
-.// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBKSNHZLH83oVbjJpViVuT78SnFDGRtDm8",
-  authDomain: "studio-2110543003-5c03f.firebaseapp.com",
-  projectId: "studio-2110543003-5c03f",
-  storageBucket: "studio-2110543003-5c03f.firebasestorage.app",
-  messagingSenderId: "944057727202",
-  appId: "1:944057727202:web:7aafa451dbcd2a238d46bb"
+.const firebaseConfig = {
+    apiKey: "AIzaSyBKSNHZLH83oVbjJpViVuT78SnFDGRtDm8",
+    authDomain: "studio-2110543003-5c03f.firebaseapp.com",
+    projectId: "studio-2110543003-5c03f",
+    storageBucket: "studio-2110543003-5c03f.firebasestorage.app",
+    messagingSenderId: "944057727202",
+    appId: "1:944057727202:web:7aafa451dbcd2a238d46bb"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
